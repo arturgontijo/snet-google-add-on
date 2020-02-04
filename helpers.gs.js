f37d899b96c63e4f7f9cbdd64f948c14be937f12
arturgontijo <arturgontijo@gmail.com>
2020-02-04 15:44:20 -0300

    Version 0.2 - Calling Prophet Service.

--- helpers.gs.js
+++ helpers.gs.js
@@ -19,42 +19,138 @@ function showSidebar() {
   SpreadsheetApp.getUi().showSidebar(html);
 };
 
-function WriteColumns(data, words, next_letter){
+function WriteColumns(series, response){
   var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
   var resultsheet = spreadsheet.getSheetByName("Result");
   if (resultsheet != null) {
-        spreadsheet.deleteSheet(resultsheet);
-    }
-
+    spreadsheet.deleteSheet(resultsheet);
+  }
   resultsheet = spreadsheet.insertSheet();
   resultsheet.setName("Result");
-  resultsheet.getRange(1, 1).setValue("Close");
-  resultsheet.getRange(1, 2).setValue("SAX Word");
+  resultsheet.appendRow([ "Date", "Series", "Trend", "Seasonal", "Forecast", "Lower", "Upper"])
   
-  for(var i=0; i<data.length; i++){
-    resultsheet.getRange(i+2, 1).setValue(data[i]);
-    if(i < words.length) {
-      resultsheet.getRange(i+2, 2).setValue(words[i]);
-    }
+  var chartssheet = spreadsheet.getSheetByName("Charts");
+  if (chartssheet != null) {
+    spreadsheet.deleteSheet(chartssheet);
   }
+  chartssheet = spreadsheet.insertSheet();
+  chartssheet.setName("Charts");
 
-  var range = resultsheet.getRange("A1:A")
-  var chart = resultsheet.newChart()
-      .addRange(range)
-      .setOption('title', 'Chart - Forecast: ' + next_letter)
-      .setPosition(3, 3, 0, 0)
+  // STL
+  var trend = response.trend;
+  var seasonal = response.seasonal;
+  // Prophet
+  var forecast_ds = response.forecast_ds;
+  var forecast = response.forecast;
+  var forecast_lower = response.forecast_lower;
+  var forecast_upper = response.forecast_upper;
+  
+  var r = [];
+  for(var i=0; i < series.length; i++){
+    r.push([forecast_ds[i],
+            series[i],
+            trend[i],
+            seasonal[i],
+            forecast[i],
+            forecast_lower[i],
+            forecast_upper[i]
+           ])
+  }
+  // Adding the Forecast Points
+  for(var i=series.length; i < forecast.length; i++){
+    r.push([forecast_ds[i],
+            "",
+            "",
+            "",
+            forecast[i],
+            forecast_lower[i],
+            forecast_upper[i]
+           ])
+  }
+  var range = resultsheet.getRange(2, 1, forecast.length, 7);
+  range.setValues(r);
+
+  var range_f_series = resultsheet.getRange("B1:B")
+  var chart_f_series = resultsheet.newChart()
+      .setChartType(Charts.ChartType.LINE)
+      .addRange(range_f_series)
+      .setOption('title', 'Series & Forecast')
+      .setOption('width', 1000)
+      .setOption('height', 420)
+      .setPosition(2, 1, 0, 0)
       .build();
-
-  resultsheet.insertChart(chart);
+  var range_forecast = resultsheet.getRange("E1:E")
+  chart_f_series = chart_f_series.modify()
+      .addRange(range_forecast)
+      .build();
+  var range_f_lower = resultsheet.getRange("F1:F")
+  chart_f_series = chart_f_series.modify()
+      .addRange(range_f_lower)
+      .build();
+  var range_f_upper = resultsheet.getRange("G1:G")
+  chart_f_series = chart_f_series.modify()
+      .addRange(range_f_upper)
+      .setOption('series',
+                 {
+                   0:{labelInLegend:"Series", color: '#000000'},
+                   1:{labelInLegend:"Forecast", color: '#0000ff', opacity: 0},
+                   2:{labelInLegend:"Lower", color: '#aff2fd', lineDashStyle: [4, 1], opacity: 0},
+                   3:{labelInLegend:"Upper", color: '#aff2fd', lineDashStyle: [4, 1], opacity: 0}
+                 }
+                )
+      .build();
+  
+//  var range_series = resultsheet.getRange("B1:B")
+//  var chart_series = resultsheet.newChart()
+//      .addRange(range_series)
+//      .setOption('title', 'Series')
+//      .setOption('width', 1000)
+//      .setOption('height', 420)
+//      .setPosition(22, 1, 0, 0)
+//      .build();
+//  
+//  var range_trend = resultsheet.getRange("C1:C")
+//  var chart_trend = resultsheet.newChart()
+//      .addRange(range_trend)
+//      .setOption('title', 'Trend')
+//      .setOption('width', 1000)
+//      .setOption('height', 420)
+//      .setPosition(42, 1, 0, 0)
+//      .build();
+//  
+//  var range_seasonal = resultsheet.getRange("D1:D")
+//  var chart_seasonal = resultsheet.newChart()
+//      .addRange(range_seasonal)
+//      .setOption('title', 'Seasonal')
+//      .setOption('width', 1000)
+//      .setOption('height', 420)
+//      .setPosition(62, 1, 0, 0)
+//      .build();
+  
+  chartssheet.insertChart(chart_f_series);
+//  chartssheet.insertChart(chart_series);
+//  chartssheet.insertChart(chart_trend);
+//  chartssheet.insertChart(chart_seasonal);
   
   return true;
 };
 
 function getSelection(){
   var spreadsheet = SpreadsheetApp.getActive();
-  var activerange = spreadsheet.getActiveRange().getValues();
-  if(activerange) {
-    var data = activerange.join().split(',').filter(Boolean);
+  var selection = spreadsheet.getSelection();
+  if(selection) {
+    var data = [];
+    var values = [];
+    var ranges =  selection.getActiveRangeList().getRanges();
+    for (var i = 0; i < ranges.length; i++) {
+      values = ranges[i].getValues();
+      // Removing header
+      values.shift();
+      data.push(values.join().split(',').filter(Boolean));
+    }
+    for (var i = 0; i < data[0].length; i++) {
+      data[0][i] = Utilities.formatDate(new Date(data[0][i]), "GMT", "yyyy-MM-dd")
+    }
     return data;
   } else {
     return [];
